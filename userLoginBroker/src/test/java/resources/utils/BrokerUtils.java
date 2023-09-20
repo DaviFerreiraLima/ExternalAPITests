de38ffe57d0a033e2e0c44061524dadcc947ce13
RyanPatrickBig <115416125+RyanPatrickBig@users.noreply.github.com>
2023-09-19 21:42:28 -0300

    Adding the rest of the test

--- userLoginBroker/src/test/java/resources/utils/BrokerUtils.java
+++ userLoginBroker/src/test/java/resources/utils/BrokerUtils.java
@@ -26,4 +26,28 @@ public class BrokerUtils {
 
         return broker;
     }
+    
+    public static JsonObject criarPostBrokerInvalido(){
+    	JsonObject broker = new JsonObject();
+
+        broker.addProperty("user_id", "");
+        broker.addProperty("cleansession", true);
+        broker.addProperty("name", "");
+        broker.addProperty("description", "Description for John Doe");
+        broker.addProperty("broker_port", 0);
+        broker.addProperty("broker_host", "mqtt.example.com");
+        broker.addProperty("username", "john_doe");
+        broker.addProperty("password", "mySecretPassword");
+        broker.addProperty("lastwilltopic", "lastwill_topic");
+        JsonObject lastwillqos = new JsonObject();
+        lastwillqos.addProperty("type", 1);
+        lastwillqos.addProperty("enum", "AT_LEAST_ONCE");
+        broker.add("lastwillqos", lastwillqos);
+        broker.addProperty("lastwillmessage", "Last Will Message");
+        broker.addProperty("lastwillretain", true);
+        broker.addProperty("keepalive", 300);
+
+        return broker;
+    }
+
 }
